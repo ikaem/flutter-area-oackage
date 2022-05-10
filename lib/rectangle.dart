f0ef5f4cefe099c0f51c3ab18a63663d5b7c1864
unknown <marinovic.karlo@gmail.com>
2022-05-10 18:40:44 +0200

    initial

---
+++ lib/rectangle.dart
@@ -0,0 +1,8 @@
+part of area;
+
+String calculateAreaRect(double width, double height) {
+  double result = width * height;
+  final formatter = NumberFormat("#.####");
+
+  return formatter.format(result);
+}
